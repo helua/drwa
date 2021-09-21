32bb307b2098fa60b63c1fad13ffb556556c8c25
helua <helua@users.noreply.github.com>
2021-09-21 15:00:05 +0200

    shrink menu test

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  @ViewChild('stickyMenu') menuElement: ElementRef;
+
+  onScroll: boolean = false;
+  // elementPosition: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
+  // ngAfterViewInit(){
+  //   this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  // }
+
+  @HostListener('window:scroll', ['$event'])
+    handleScroll(){
+      const windowScroll = window.pageYOffset;
+      const menu = document.getElementById("nav-main") as HTMLElement;
+      if(windowScroll >= 160){
+        this.onScroll = true;
+        console.log(windowScroll, this.onScroll)
+        menu.style.top = "0";
+      } else {
+        this.onScroll = false;
+        console.log(windowScroll, this.onScroll)
+        menu.style.top = "10px";
+
+
+      }
+    }
 
 }
